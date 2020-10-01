acf567dfc56f16d3f7f0553875954942173ef364
Kyle Laker <kyle@laker.email>
2020-10-01 18:04:43 -0400

    Add additional helper functions

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,28 @@
+function Set-AwsProfile {
+    param(
+        [string]$ProfileName
+    )
+
+    $env:AWS_PROFILE = $ProfileName
+}
+
+function Get-AwsProfile {
+    Write-Host $env:AWS_PROFILE
+}
+
+function Out-Edge {
+    [CmdletBinding()]
+    Param(
+        [ValidateNotNullOrEmpty()]
+        [string]$FilePath = ".\index.html"
+    )
+
+    $FullPath = (Convert-Path -path $Filepath).Replace('\', '/')
+    $Uri = "file:///$FullPath"
+    Write-Verbose $Uri
+    Start-Process "msedge" "$Uri"
+}
+
 function Set-Export {
     [CmdletBinding()]
     param (
